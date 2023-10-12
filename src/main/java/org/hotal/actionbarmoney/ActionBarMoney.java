3e445cfe90b5e8bac7a7733d811eccd3eeebccb0
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-12 17:12:30 +0900

    Initial Commit

---
+++ src/main/java/org/hotal/actionbarmoney/ActionBarMoney.java
@@ -0,0 +1,17 @@
+package org.hotal.actionbarmoney;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ActionBarMoney extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
