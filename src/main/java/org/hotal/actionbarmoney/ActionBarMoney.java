0d48b39c75c62cd1f7254a7d21f27b5dfa38ce9f
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-12 17:56:26 +0900

    first commit

--- src/main/java/org/hotal/actionbarmoney/ActionBarMoney.java
+++ src/main/java/org/hotal/actionbarmoney/ActionBarMoney.java
@@ -1,13 +1,48 @@
 package org.hotal.actionbarmoney;
-
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import net.milkbowl.vault.economy.Economy;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 
-public final class ActionBarMoney extends JavaPlugin {
+public class ActionBarMoney extends JavaPlugin implements Listener {
 
+    private Economy econ =null;
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        if (!setupEconomy() ) {
+            getLogger().severe(String.format("[%s] - Vaultの経済プラグインが見つかりません! 無効化しています...", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        displayMoney(player);
+    }
+
+    private void displayMoney(Player player) {
+        double balance = econ.getBalance(player);
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR,TextComponent.fromLegacyText("§e現在の所持金"+balance+""+econ.currencyNamePlural()));
     }
 
     @Override
